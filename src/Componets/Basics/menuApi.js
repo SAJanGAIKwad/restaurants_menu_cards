0400899769dad05adbed361add25aaf207cfe99c
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2023-02-05 18:46:22 +0530

    first

---
+++ src/Componets/Basics/menuApi.js
@@ -0,0 +1,88 @@
+const Menu = [
+    {
+    id:1,
+    image:"images/hero.jpeg",
+    name:'maggi',
+    category:"breackfast",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+},
+{
+    id:2,
+    image:"././images/hero.jpeg",
+    name:'Fitzaa',
+    category:"Lunch",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+},
+{
+    id:3,
+    image:"././images/hero.jpeg",
+    name:"maggi",
+    category:"Evening",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+},
+{
+    id:4,
+    image:"././images/hero.jpeg",
+    name:"Pitzza",
+    category:"Dinner",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+},
+{
+    id:5,
+    image:"././images/hero.jpeg",
+    name:"Wine",
+    category:"Dinner",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+},
+{
+    id:6,
+    image:"././images/hero.jpeg",
+    name:"Chapati",
+    category:"Dinner",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+
+},
+{
+    id:7,
+    image:"././images/hero.jpeg",
+    name:"idali",
+    category:"breackfast",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+
+},
+{
+    id:8,
+    image:"././images/hero.jpeg",
+    name:"Tea",
+    category:"Evening",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+
+},
+{
+    id:9,
+    image:"././images/hero.jpeg",
+    name:"Riceplate",
+    category:"Lunch",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+
+},
+{
+    id:9,
+    image:"././images/hero.jpeg",
+    name:"Riceplate",
+    category:"Box",
+    price:"12rs",
+    description:"loremfdsfsa dfd ffbfdkfb fbg vgfbgdvfgkdf gvdfbgksdfgb fdgbdfgbdfhgbfds fhgbf gfg",
+
+},
+];
+export default Menu;
